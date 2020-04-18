6c9d5dcc1d61165da6c3b32b7363243789d29e7b
eunery <asdad@yaofj.com>
2020-04-18 16:24:04 +0500

    8 empty levels.

---
+++ AssembledGame/AssembledGame/Level4.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AssembledGame
+{
+    /// <summary>
+    /// Логика взаимодействия для Level4.xaml
+    /// </summary>
+    public partial class Level4 : Window
+    {
+        public Level4()
+        {
+            InitializeComponent();
+        }
+    }
+}
