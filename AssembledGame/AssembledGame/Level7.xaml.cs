8fcd2a7a5cedfe841f7a1b00389aa21fd02928f3
eunery <asdad@yaofj.com>
2020-05-02 14:56:17 +0500

    7 levels.

--- AssembledGame/AssembledGame/Level7.xaml.cs
+++ AssembledGame/AssembledGame/Level7.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,17 +12,61 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace AssembledGame
 {
-    /// <summary>
-    /// Логика взаимодействия для Level7.xaml
-    /// </summary>
     public partial class Level7 : Window
     {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+       
         public Level7()
         {
             InitializeComponent();
+            Answer.Click += Button_Click;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (mediaPlayer.Source != null)
+                lblStatus.Content = String.Format("{0} / {1}", mediaPlayer.Position.ToString(@"mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+            else
+                lblStatus.Content = "No file selected...";
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            Level8 lvl8 = new Level8();
+            lvl8.Show();
+            LevelSeven.Close();
         }
     }
 }
